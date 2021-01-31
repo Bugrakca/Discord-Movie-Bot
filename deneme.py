869be42424fa36cc42a8b40fbdbc7abe572e655f
Bugrakca <bugrakca@gmail.com>
2021-01-31 20:44:55 +0300

    Adding clear command and Youtube trailer link

---
+++ deneme.py
@@ -0,0 +1,7 @@
+import urllib.request
+import re
+
+search_keyword="blue+velvet+trailer"
+html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + search_keyword)
+video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
+print("https://www.youtube.com/watch?v=" + video_ids[0])
